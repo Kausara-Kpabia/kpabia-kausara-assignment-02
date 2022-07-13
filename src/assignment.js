d591fd2be0dd98540df087c9dc6e62b86f39482f
Kausara Kpabia <42814179+Kausara-Kpabia@users.noreply.github.com>
2022-07-13 22:04:45 +0000

    Completed Challenge 1 and 2.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let total = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    total += arrayOfNumbers[i] ;
+  }
+  
+    return total;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ const evenNumber = [];
+
+ arrayOfNumbers.forEach(number => {
+  if (number % 2 === 0) {
+    evenNumber.push(number);
+  }
+});
+
+  return evenNumber.length ;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
